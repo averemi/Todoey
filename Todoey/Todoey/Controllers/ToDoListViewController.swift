aa141968a7c15748053d2c7f4b7f879cbe122199
Anastasia Veremiichyk <anastasia.veremi@gmail.com>
2018-10-27 17:37:24 +0300

    code shortened

--- Todoey/Todoey/Controllers/ToDoListViewController.swift
+++ Todoey/Todoey/Controllers/ToDoListViewController.swift
@@ -33,9 +33,9 @@ class ToDoListViewController: UITableViewController {
         
         
         
-   //     if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
-   //         itemArray = items
-   //     }
+        if let items = defaults.array(forKey: "ToDoListArray") as? [Item] {
+            itemArray = items
+        }
         
     }
     
@@ -48,7 +48,11 @@ class ToDoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row].title
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done == true ? .checkmark : .none
         
         return cell
     }
@@ -57,11 +61,9 @@ class ToDoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        tableView.reloadData()
         
         tableView.deselectRow(at: indexPath, animated: true)
         
